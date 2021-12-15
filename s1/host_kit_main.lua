da434c4dcf97af4525d0f1b30f88dee30406d095
Ian <66448861+IanGaunt@users.noreply.github.com>
2021-12-15 10:40:12 -0800

    Update host_kit_main.lua

--- s1/host_kit_main.lua
+++ s1/host_kit_main.lua
@@ -1,1 +1,37 @@
+-- [ service ] --
+local Players, RunService = game:GetService("Players"), game:GetService("RunService")
 
+-- [ objects ] --
+local HostEvent = game.ReplicatedStorage.HostEvent
+
+-- [ variables ] --
+local whitelist = {}
+
+-- [ functions ] --
+--// remove gui from players who don't have access
+Players.PlayerAdded:Connect(function(plr)
+    local gui = plr.PlayerGui.host_kit
+    if table.find(whitelist, plr.Name) then
+      gui.Enabled = true
+    else 
+      gui:Destroy())
+    end
+end)
+
+--// checks host event for any blacklisted players
+HostEvent.OnServerEvent:Connect(function(plr, ev)
+	if not table.find(whitelist, plr.Name) then
+		plr:Kick("Please do not exploit.")
+	else
+		-- run the code required from said HostEvent
+	end
+end)
+
+--// check every frame to make sure it's not there
+RunService.Heartbeat:Connect(function()
+  for _, p in pairs (Players:GetPlayers()) do 
+    if p.PlayerGui:FindFirstChild("host_kit") and not table.find(whitelist, plr.Name) then
+      p.PlayerGui.host_kit:Destroy()
+    end
+  end
+end)
